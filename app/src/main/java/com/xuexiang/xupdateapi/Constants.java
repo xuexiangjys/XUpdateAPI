2bc059e09903b52ba258e558995cc89aaff4e1d4
xuexiangjys <xuexiangjys@163.com>
2021-01-15 23:19:20 +0800

    提交内容

--- app/src/main/java/com/xuexiang/xupdateapi/Constants.java
+++ app/src/main/java/com/xuexiang/xupdateapi/Constants.java
@@ -23,8 +23,9 @@ package com.xuexiang.xupdateapi;
  */
 public final class Constants {
 
-    public static String UPDATE_DEFAULT_URL = "https://gitee.com/xuexiangjys/XUpdate/raw/master/jsonapi/update_test.json";
+    public static final String UPDATE_DEFAULT_URL = "https://gitee.com/xuexiangjys/XUpdate/raw/master/jsonapi/update_test.json";
 
-    public static String UPDATE_FORCE_URL = "https://gitee.com/xuexiangjys/XUpdate/raw/master/jsonapi/update_forced.json";
+    public static final String UPDATE_FORCE_URL = "https://gitee.com/xuexiangjys/XUpdate/raw/master/jsonapi/update_forced.json";
 
+    public static final String UPDATE_DOWNLOAD_URL = "https://gitee.com/xuexiangjys/Resource/raw/master/jsonapi/download_test.json";
 }
